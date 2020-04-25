7041b56eed6cdc072835837f698dad5c16549288
Mojgan360 <mojgan360@gmail.com>
2020-04-26 01:38:57 +0200

    front/ Add Register form

---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,84 @@
+import React from "react";
+
+const Register = () => {
+  const [formData, setFormData] = React.useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
+  const { name, email, password, password2 } = formData;
+  const handelOnChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+  const handelOnsubmit = (e) => {
+    e.preventDefault();
+    if (password !== password2) {
+      console.log("Password not match");
+    } else {
+      console.log(formData);
+    }
+  };
+
+  return (
+    <React.Fragment>
+      <section className="container">
+        <h1 className="large text-primary">Sign Up</h1>
+        <p className="lead">
+          <i className="fas fa-user"></i> Create Your Account
+        </p>
+        <form className="form" onSubmit={(e) => handelOnsubmit(e)}>
+          <div className="form-group">
+            <input
+              type="text"
+              placeholder="Name"
+              name="name"
+              value={name}
+              onChange={(e) => handelOnChange(e)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="email"
+              placeholder="Email Address"
+              name="email"
+              value={email}
+              onChange={(e) => handelOnChange(e)}
+            />
+            <small className="form-text">
+              This site uses Gravatar so if you want a profile image, use a
+              Gravatar email
+            </small>
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={password}
+              onChange={(e) => handelOnChange(e)}
+              minLength="6"
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              placeholder="Confirm Password"
+              name="password2"
+              value={password2}
+              onChange={(e) => handelOnChange(e)}
+              minLength="6"
+            />
+          </div>
+          <input type="submit" className="btn btn-primary" value="Register" />
+        </form>
+        <p className="my-1">
+          Already have an account? <a href="login.html">Sign In</a>
+        </p>
+      </section>
+    </React.Fragment>
+  );
+};
+
+export default Register;
