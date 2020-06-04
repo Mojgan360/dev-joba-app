72b09a0d218ffd554ad6e170c017b87744b2e47a
Mojgan360 <mojgan360@gmail.com>
2020-06-04 16:02:59 +0200

    Completed post

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,7 @@ import AddExperience from "./components/profile-forms/AddExperience";
 import AddEducation from "./components/profile-forms/AddEducation";
 import Profiles from "./components/profiles/Profiles";
 import Profile from "./components/profile/Profile";
+import Posts from "./components/posts/Posts";
 
 // Redux
 import { Provider } from "react-redux";
@@ -52,21 +53,26 @@ const App = () => {
                 path="/create-profile"
                 component={CreateProfile}
               />
+
               <PrivateRoute
                 exact
                 path="/edit-profile"
                 component={EditProfile}
               />
+
               <PrivateRoute
                 exact
                 path="/add-experience"
                 component={AddExperience}
               />
+
               <PrivateRoute
                 exact
                 path="/add-education"
                 component={AddEducation}
               />
+
+              <PrivateRoute exact path="/posts" component={Posts} />
             </Switch>
           </section>
         </Fragment>
--- server.js
+++ server.js
@@ -17,6 +17,6 @@ app.get("/", (req, res) => {
 app.use("/api/user", require("./routes/api/user"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
-app.use("/api/post", require("./routes/api/post"));
+app.use("/api/posts", require("./routes/api/post"));
 
 app.listen(PORT, () => console.log(`server started on port: ${PORT}`));
