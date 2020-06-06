f6aad1dfec5fe2ba5efb2cdf88e7a9642657c1c3
Mojgan360 <mojgan360@gmail.com>
2020-06-06 15:30:55 +0200

    completed comment

--- client/src/action/post.js
+++ client/src/action/post.js
@@ -154,3 +154,4 @@ export const deleteComment = (postId, commentId) => async (dispatch) => {
     });
   }
 };
+//
--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const config = require("config");
-const db = config.get("MongoURT");
+const db = config.get("MongoURL");
 
 const connectDB = async () => {
   try {
